2a6c499c35211b08b369133d6e23cd495e62f688
Dolphin30s <98946595+Dolphin30s@users.noreply.github.com>
2022-05-26 17:14:37 +0300

    Add files via upload

---
+++ Lama-dev-reacr/src/App.js
@@ -0,0 +1,20 @@
+import './styles.css'
+import Home from './pages/Home'
+import ProductList from './pages/ProductList'
+import Product from './pages/Product'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      {/* <ProductList /> */}
+      {/* <Product /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Cart />
+    </div>
+  )
+}
